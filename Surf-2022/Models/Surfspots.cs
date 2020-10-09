108f89f1f1cdec7f2f2b89cab59a46f799e7f253
rune7536 <rune7536edu.eal.dk>
2020-10-09 14:56:19 +0200

    MVC

---
+++ Surf-2022/Models/Surfspots.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Surf_2022.Models
+{
+    public class Surfspots
+    {
+		public int Id { get; set; }
+		public string Name { get; set; }
+        public float Lattitude { get; set; }
+        public float Longtitude { get; set; }
+    }
+}
