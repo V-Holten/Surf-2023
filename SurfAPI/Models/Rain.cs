78771b297d62a9afb69efd567fc853e1efd6f24f
Jeremy Howard Andersen <jere0015@edu.ucl.dk>
2020-11-27 10:58:14 +0100

    Tilføjet Models og dens vejr-objekter

---
+++ SurfAPI/Models/Rain.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SurfAPI.Models
+{
+    public class Rain
+    {
+        public double _1h { get; set; }
+    }
+}
